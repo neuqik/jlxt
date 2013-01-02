a9c663636c954d919098f6e847a34f0f4471f2ec
neuqik <neuqik@gmail.com>
2013-01-02 10:00:03 +0800

    add

---
+++ src/main/java/net/biz/hr/vo/HRD_EMP_FAMILY.java
@@ -0,0 +1,77 @@
+package net.biz.hr.vo;
+
+public class HRD_EMP_FAMILY {
+	private String ID;
+	private String EMP_ID;
+	private String RELATION_NAME;
+	private String RELATION_TYPE;
+	private String RELATION_TEL;
+	private String WORKDEPT;
+	private String WORKROLE;
+	private String MEMO;
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public String getEMP_ID() {
+		return EMP_ID;
+	}
+
+	public void setEMP_ID(String eMP_ID) {
+		EMP_ID = eMP_ID;
+	}
+
+	public String getRELATION_NAME() {
+		return RELATION_NAME;
+	}
+
+	public void setRELATION_NAME(String rELATION_NAME) {
+		RELATION_NAME = rELATION_NAME;
+	}
+
+	public String getRELATION_TYPE() {
+		return RELATION_TYPE;
+	}
+
+	public void setRELATION_TYPE(String rELATION_TYPE) {
+		RELATION_TYPE = rELATION_TYPE;
+	}
+
+	public String getWORKDEPT() {
+		return WORKDEPT;
+	}
+
+	public void setWORKDEPT(String wORKDEPT) {
+		WORKDEPT = wORKDEPT;
+	}
+
+	public String getWORKROLE() {
+		return WORKROLE;
+	}
+
+	public void setWORKROLE(String wORKROLE) {
+		WORKROLE = wORKROLE;
+	}
+
+	public String getMEMO() {
+		return MEMO;
+	}
+
+	public void setMEMO(String mEMO) {
+		MEMO = mEMO;
+	}
+
+	public String getRELATION_TEL() {
+		return RELATION_TEL;
+	}
+
+	public void setRELATION_TEL(String rELATION_TEL) {
+		RELATION_TEL = rELATION_TEL;
+	}
+
+}
